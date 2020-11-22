4589e49d4519a5e8ced1d78fe067a166b2352fba
yuusuine <yuuki.990510@gmail.com>
2020-11-22 23:02:06 +0900

    udpate RenderingObject

--- RenderingObject.cpp
+++ RenderingObject.cpp
@@ -4,9 +4,7 @@ RenderingObject::RenderingObject() {
 
 }
 void RenderingObject::draw(VkCommandBuffer command_buffer, bool is_bound_buffers) {
-	if (is_bound_buffers) {
-		this->model.draw(command_buffer);
-	}
+	this->model.draw(command_buffer, is_bound_buffers);
 }
 void RenderingObject::loadModel(std::string file_path) {
 	this->model.load(file_path);
